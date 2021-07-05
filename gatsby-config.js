682fa4454adb5c2bc10809b304abd88a8d91c3fd
higeknuckle <y.hatomin@gmail.com>
2021-07-05 10:46:57 +0000

    WIP

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Gatsby Tutorial",
+  },
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
